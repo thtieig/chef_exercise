a1f9f08244db5e266177ecac9e5c52cfc5a8e913
ChrisTian <thtieig@gmail.com>
2017-04-20 11:28:35 +0100

    Restored default.rb after push issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,51 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-# Installing basic packages
-package ['vim', 'cowsay', 'firewalld']
+
+# Apache/PHP setup
+package ['httpd', 'php', 'php-xml', 'php-pear', 'php-gd', 'php-cli', 'php-soap', 'php-pdo', 'php-mysql']
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.php' do
+  source 'index.php.erb'
+  mode "0644"
+end
+
+# Firewall setup
+package 'firewalld'
 
 service 'firewalld' do
   action [:enable, :start]
 end
 
-execute 'firewall_ssh' do
-  command '/usr/bin/firewall-cmd  --permanent --zone public --add-service ssh'
-  ignore_failure true
+firewalld_service 'http' do
+    action :add
+    zone   'public'
+end
+
+firewalld_service 'https' do
+    action :add
+    zone   'public'
+end
+
+firewalld_service 'ssh' do
+    action :add
+    zone   'public'
 end
 
 service 'firewalld' do
   action [:restart]
+end
+
+
+
+
+# Database setup
+package ['mariadb-server', 'mariadb']
+
+service 'mariadb' do
+  action [:enable, :start]
 end
